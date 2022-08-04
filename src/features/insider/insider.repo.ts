1fc515b40ba37d894d57a3c2be5f276050a31748
HM <hieumdd@gmail.com>
2022-08-04 16:33:40 +0700

    feat: Remove partner name

--- src/features/insider/insider.repo.ts
+++ src/features/insider/insider.repo.ts
@@ -5,7 +5,7 @@ import { UpsertDto } from './insider.dto';
 const client = axios.create({
     baseURL: 'https://unification.useinsider.com/api/',
     headers: {
-        'X-PARTNER-NAME': process.env.INSIDER_PARTNER_NAME || '',
+        'X-PARTNER-NAME': 'vuanem',
         'X-REQUEST-TOKEN': process.env.INSIDER_REQUEST_TOKEN || '',
     },
 });
