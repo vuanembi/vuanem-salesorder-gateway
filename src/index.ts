0fb4a35d93373e64ebb87d7ee6694488a5f1bd14
HM <hieumdd@gmail.com>
2022-08-04 15:14:44 +0700

    feat: Add Klaviyo to index

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,18 @@ import express from 'express';
 
 import { SalesOrder } from './features/sales-order.dto';
 import * as InsiderService from './features/insider/insider.service';
+import * as KlaviyoService from './features/klaviyo/klaviyo.service';
 
 const app = express();
 
 app.post('/', (req, res) => {
     const { body }: { body: SalesOrder } = req;
 
-    Promise.all([InsiderService.upsert].map((service) => service(body)))
+    Promise.all(
+        [InsiderService.upsert, KlaviyoService.track].map((service) =>
+            service(body),
+        ),
+    )
         .then(() => res.status(200).json({ ok: true }))
         .catch((err) => res.status(500).json({ err }));
 });
